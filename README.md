fbdce779b03990b5f87a6fe06b5af4e9b883d4e6
Hoseung Choi <hopung@gmail.com>
2020-11-25 13:39:16 +0900

    package configuration

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # astroBF
 Astronomical images BeautiFied
+
+This package is intended to be a part of a *neural tone mapper* project. 
+I will explore variety of modern tone mapping operators and come up with a learnable model. 
+That is, early commit will mostly contain pedagogical examples.
---
+++ setup.py
@@ -0,0 +1,30 @@
+"""A setuptools based setup module. 
+    They say, "Always prefer setuptools over distutils
+
+    See:
+    https://github.com/pypa/sampleproject
+    https://github.com/pypa/sampleproject/blob/master/setup.py
+"""
+
+from setuptools import setup, find_packages
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
+
+long_description = (here / "README.md").read_text(encoding='utf-8')
+
+setup(
+    name='astrobf',
+    version='0.0.1',
+    description='Tone mapping for astronomical images',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/Hoseung/astroBF',
+    classifiers=['Programming Language :: Python :: 3'],
+    package_dir={'': 'src'},
+    packages=find_packages(where='src'),
+    python_requires='>=3.7',
+    install_requires=['numpy', 'scikit-image', 'astropy'],
+    extras_require={'dev':[''],
+                    'test':['']},
+)
